750cb43d44acc360c94de507ce06fe86f3e179f9
riber <riber@CarlosMSI>
2021-06-01 22:21:56 -0500

    ep4

--- AsteroidsPOO/src/graphics/Loader.java
+++ AsteroidsPOO/src/graphics/Loader.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package graphics;
 
-/**
- *
- * @author andyl
- */
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Loader {
-    
+	
+	public static BufferedImage ImageLoader(String path)
+	{
+		try {
+			return ImageIO.read(Loader.class.getResource(path));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 }
