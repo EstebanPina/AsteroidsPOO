c0a3ca4c1fb25362c1b05c31e53082dc970f69cb
Carlos Briones <jbriones.isc@icloud.com>
2021-06-02 21:40:56 -0500

    1stcommitEP5

---
+++ AsteroidsPOO/src/gameObjets/GameObject.java
@@ -0,0 +1,8 @@
+package gameObjets;
+
+import java.awt.image.BufferedImage;
+
+public abstract class GameObject {
+    protected BufferedImage texture;
+     
+}
---
+++ AsteroidsPOO/src/states/GameState.java
@@ -0,0 +1,15 @@
+package states;
+
+import java.awt.Graphics;
+
+public class GameState {
+    public GameState(){
+
+    }
+    public void update(){
+
+    }
+    public void draw(Graphics g){
+        
+    }
+}
